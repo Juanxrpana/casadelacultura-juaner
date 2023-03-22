2a045fc2ad2ab3f5005492c617155706f8c6d36f
Juanxrpana <jo.hw722@gmail.com>
2023-03-21 22:37:00 -0400

    nuevo pdf2

--- js/notificaciones.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#noti").click(function() {
-        $("#notificacion").toggle(function() {
-            $(this).toggleClass("desplazar");
-        });
-    });
-});
