23301a11cb1371067d3622021f9ff62ea0aa3ec6
Juanxrpana <jo.hw722@gmail.com>
2023-03-21 22:32:26 -0400

    nuevo pdf

--- reporte/imprimir.php
+++ reporte/imprimir.php
@@ -1,11 +1,13 @@
 <?php
 require_once '../vendor/autoload.php';
+ob_start();
+require_once ('print_view.php');
+$html = ob_get_clean();
  
 use Spipu\Html2Pdf\Html2Pdf;
 
 $html2pdf = new Html2Pdf('p','A4','es','true','UTF-8');
-$html2pdf->writeHTML('<div style="
-font-size: 48em;"><h1>Holamundo.22</h1></div>');
-$html2pdf->output('goku.pdf');
+$html2pdf->writeHTML($html);
+$html2pdf->output('REPORTE CC.pdf');
 
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '026cf58f314207b717f156a8197b00a941b721d7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '026cf58f314207b717f156a8197b00a941b721d7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'spipu/html2pdf' => array(
+            'pretty_version' => 'v5.2.7',
+            'version' => '5.2.7.0',
+            'reference' => 'b0f477711de3052041072897510e690975aa37ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../spipu/html2pdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.6.2',
+            'version' => '6.6.2.0',
+            'reference' => 'e3cffc9bcbc76e89e167e9eb0bbda0cab7518459',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
